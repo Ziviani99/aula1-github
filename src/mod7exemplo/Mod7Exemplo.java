58f951c4c6eaf45b4b44058741a52f0526953c22
Ziviani99 <ZIVIANI99@HOTMAIL.COM>
2019-01-08 17:59:08 -0200

    Mensagem de boa noite

--- src/mod7exemplo/Mod7Exemplo.java
+++ src/mod7exemplo/Mod7Exemplo.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mod7exemplo;
 
-/**
- *
- * @author zivia
- */
 public class Mod7Exemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Bom dia");
+        System.out.println("Boa tarde");
+        System.out.println("Boa noite");
     }
     
 }
