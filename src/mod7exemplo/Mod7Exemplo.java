77d1d7ea5b8c33625758f5734fc64624486e8a7d
Ziviani99 <ZIVIANI99@HOTMAIL.COM>
2019-01-08 17:10:36 -0200

    Acrescentada mensagem de bom dia

--- src/mod7exemplo/Mod7Exemplo.java
+++ src/mod7exemplo/Mod7Exemplo.java
@@ -15,7 +15,7 @@ public class Mod7Exemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bom dia");
     }
     
 }
